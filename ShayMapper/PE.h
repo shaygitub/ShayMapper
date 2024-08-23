92e521477eb50b02b25c5fbc2cbf06b6983cc37b
shaygitub <122000611+shaygitub@users.noreply.github.com>
2024-08-23 22:38:48 +0300

    Delete ShayMapper directory

--- ShayMapper/PE.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "utils.h"
-
-
-namespace PortableExecutable {
-	PIMAGE_DOS_HEADER GetDosHeader(PVOID ImageBase);
-	PIMAGE_NT_HEADERS64 GetNtHeaders(PVOID ImageBase, PIMAGE_DOS_HEADER ImageDosHeader);
-}
--- ShayMapper/elevate.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\mt.exe" -manifest "ShayMapper.exe.manifest" -outputresource:x64\Release\ShayMapper.exe";#1
